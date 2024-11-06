d2ecd48d99d81c9e183d070c35c6e8c54d993432
André <andre.aslima@hotmail.com>
2024-11-05 22:36:00 -0300

    Uso do cors implementado na API

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,25 @@
-// Importando biblioteca
 import express,{ Request, Response, NextFunction } from "express";
+import { router } from "./routes";
 import "express-async-errors"
 import "reflect-metadata";
 import "./database/index";
-
+ 
 // Importando funções de rotas
-import { router } from "./routes";
-
+const cors = require('cors')
 const app = express();
-
-// Declarando a biblioteca
-app.use(express.json()); 
-// Inicializando a função de rotas
+app.use(cors())
+app.use(express.json());
 app.use(router);
-
+ 
 app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
-        
+       
         if (err instanceof Error ){
             return response.status(400).json({
                 error: err.message,
             });
         }
-
+ 
         return response.status(500).json({
             status: "error",
             message: "Internal Server Error",
